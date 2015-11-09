602f97cc97764b7123126696f303619986815106
Afanasii Kurakin <afanasy@gmail.com>
2015-11-09 14:58:21 +0800

    added code and description

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# db3-set
+[![Linux Build][https://img.shields.io/travis/afanasy/db3-set/master.svg]][https://travis-ci.org/afanasy/db3-set]
+Transforming rules in JSON
+
+### When is this useful?
+If you want to use the same transforming rules for js and SQL `set`.
+
+### Usage
+```js
+var set = require('db3-set')
+set.query(rule)
+//returns corresponding sql `set` clause
+set.transform(rule)
+//returns js function
+```
+#### SQL set
+```js
+set.query({id: 1, name: 'Apple'})
+// returns `id` = 1, name = 'Apple'
+set.query({created: {now: true}})
+// returns `created` = now()
+set.query({rating: {'+=': 1}})
+// returns `rating` = rating + 1
+```
+
+#### Transform function
+```js
+var fruit = {name: 'Apple', rating: 1}
+set.transform({rating: 2})(fruit)
+// fruit will be
+// {name: 'Apple', rating: 2}
+set.transform({created: {now: true}})(fruit)
+// fruit will be
+// {name: 'Apple', rating: 1, created: '2015-11-09 14:45:00'}
+set.transform({rating: {'+=': 2}})(fruit)
+// fruit will be
+// {name: 'Apple', rating: 3}
+```
---
+++ test.js
@@ -0,0 +1,42 @@
+var
+  _ = require('underscore'),
+  moment = require('moment'),
+  expect = require('expect.js'),
+  set = require('./')
+
+describe('#set', function () {
+  describe('#query', function () {
+    it('translates number', function () {
+      expect(set.query(1)).to.be('`id` = 1')
+    })
+    it('translates object', function () {
+      expect(set.query({id: 1, name: 'Adam'})).to.be("`id` = 1, `name` = 'Adam'")
+    })
+    it('translates object with now', function () {
+      expect(set.query({created: {now: true}})).to.be('`created` = now()')
+    })
+    _.each(set.operator, function (operator) {
+      it('translates object with ' + operator + ' operator', function () {
+        expect(set.query({id: _.object([[operator, [1, 2]]])})).to.be('`id` = 1 ' + operator + ' 2')
+      })
+    })
+  })
+  describe('#transform', function () {
+    var fruit = {id: 1, name: 'Apple'}
+    it('transforms with number', function () {
+      expect(set.transform(2)(fruit).id).to.be(2)
+    })
+    it('transforms with object', function () {
+      expect(set.transform({id: 3})(fruit).id).to.be(3)
+    })
+    it('transforms with now', function () {
+      expect(set.transform({created: {now: true}})(fruit).created).to.be(moment().format('YYYY-MM-DD HH:mm:ss'))
+    })
+    _.each(set.operator, function (operator) {
+      it('transforms object with ' + operator + ' operator', function () {
+        fruit.id = 1
+        expect(set.transform({id: _.object([[operator, [1, 2]]])})(fruit).id).to.be(eval('1 ' + operator + ' 2'))
+      })
+    })
+  })
+})
