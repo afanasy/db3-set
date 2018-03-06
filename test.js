e57d426bbd773f232cabdd48727110a3b9dddd9d
Afanasii Kurakin <afanasy@gmail.com>
2018-03-06 14:15:11 +0300

    removed expect.js dep

--- test.js
+++ test.js
@@ -1,46 +1,46 @@
 var _ = require('underscore')
-var expect = require('expect.js')
+var assert = require('assert')
 var set = require('./')
 
 describe('#set', function () {
   describe('#query', function () {
     it('translates number', function () {
-      expect(set.query(1)).to.be('`id` = 1')
+      assert.equal(set.query(1), '`id` = 1')
     })
     it('translates object', function () {
-      expect(set.query({id: 1, name: 'Adam'})).to.be("`id` = 1, `name` = 'Adam'")
+      assert.equal(set.query({id: 1, name: 'Adam'}), '`id` = 1, `name` = \'Adam\'')
     })
     it('translates object with now', function () {
-      expect(set.query({created: {now: true}})).to.be('`created` = now()')
+      assert.equal(set.query({created: {now: true}}), '`created` = now()')
     })
     _.each(set.operator, function (operator) {
       it('translates object with ' + operator + ' operator', function () {
-        expect(set.query({id: _.object([[operator, [1, 2]]])})).to.be('`id` = 1 ' + operator + ' 2')
+        assert.equal(set.query({id: _.object([[operator, [1, 2]]])}), '`id` = 1 ' + operator + ' 2')
       })
     })
     it('does not crash on empty object', function () {
-      expect(set.query({created: {}})).to.be('`created` = \'[object Object]\'')
+      assert.equal(set.query({created: {}}), '`created` = \'[object Object]\'')
     })
   })
   describe('#transform', function () {
     var fruit = {id: 1, name: 'Apple'}
     it('transforms with number', function () {
-      expect(set.transform(2)(fruit).id).to.be(2)
+      assert.equal(set.transform(2)(fruit).id, 2)
     })
     it('transforms with object', function () {
-      expect(set.transform({id: 3})(fruit).id).to.be(3)
+      assert.equal(set.transform({id: 3})(fruit).id, 3)
     })
     it('transforms with now', function () {
-      expect(set.transform({created: {now: true}})(fruit).created).to.be((new Date(Date.now() - (new Date).getTimezoneOffset() * 60000)).toISOString().substring(0, 19).replace('T', ' '))
+      assert.equal(set.transform({created: {now: true}})(fruit).created, (new Date(Date.now() - (new Date).getTimezoneOffset() * 60000)).toISOString().substring(0, 19).replace('T', ' '))
     })
     _.each(set.operator, function (operator) {
       it('transforms object with ' + operator + ' operator', function () {
         fruit.id = 1
-        expect(set.transform({id: _.object([[operator, [1, 2]]])})(fruit).id).to.be(eval('1 ' + operator + ' 2'))
+        assert.equal(set.transform({id: _.object([[operator, [1, 2]]])})(fruit).id, eval('1 ' + operator + ' 2'))
       })
     })
     it('does not crash on empty object', function () {
-      expect(set.transform({created: {}})(fruit).created).to.eql({})
+      assert.deepEqual(set.transform({created: {}})(fruit).created, {})
     })
   })
 })
